32e730a955d12c7c305a87a4e6784e00d296c750
Thang Tran <thangtran@brodev.com>
2021-11-27 09:41:21 +0700

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,27 @@
+from brownie import network, config, accounts, MockV3Aggregator
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENT = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENT = ["development", "ganache-local"]
+DECIMALS = 8
+STARTING_PRICE = 20000000000
+
+
+def get_account():
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENT
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENT
+    ):
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying mocks...")
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": get_account()}
+        )
+    print("Mocks deployed")
